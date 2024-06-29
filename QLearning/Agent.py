296ac12a153cfd722fa1d66784775353f9b359ef
ManuOrdz <manuelordzslv@gmail.com>
2024-06-28 20:52:05 -0500

    fix errors

--- QLearning/Agent.py
+++ QLearning/Agent.py
@@ -10,10 +10,12 @@ class QLearning:
         initial_epsilon: float,
         epsilon_decay: float,
         final_epsilon: float,
-        q_size: tuple = (10,10,3),
+        observation_space: gym.spaces.box.Box,
+        action_space: gym.spaces.space.discrete.Discrete,
+        q_size: tuple = (10,10),
         gamma: float = 0.95,
     ):
-        """Initialize a Reinforcement Learning agent with an empty dictionary
+        """Initialize a Reinforcement Learning agent with an zero array
         of state-action table (q_table), a learning rate an epsilon.
         
         Args:
@@ -21,49 +23,59 @@ class QLearning:
             learning_rate (float): The learning rate
             initial_epsilon: The inital epsilon value
             epsilon_decay: The decay for epsilon
+            observation_space: The observation space
+            action_space: The action space
             final_epsilon: The final epsilon value
             size_table: Size of the Q-table (observation_size, actions)
-            discount_factor: The discount factor for computing the Q-value
+            gamma: The discount factor for computing the Q-value
         """
         self.lr = learning_rate
         self.epsilon = initial_epsilon
         self.epsilon_decay = epsilon_decay
         self.final_epsilon = final_epsilon
-        
-        self.q_size = q_size        
+        self.observation_space = observation_space
+        self.action_space = action_space
+        self.q_size = q_size   
         self.gamma = gamma
-        self.training_error = []
         self.reset_qtable()
+        self.training_error = []
     
-    def map_obs_to_stat(self, obs_value: tuple, obs_max:tuple, obs_min: tuple) -> tuple:
-        """ Discretize observation value
+    def map_obs_to_stat(self, obs_value) -> tuple:
+        """ Discretize observation value and map to a state value
 
         Args:
             obs_value (float): observation_value
         """        
+        obs_min = self.observation_space.low
+        obs_max = self.observation_space.high
         aux = (obs_value - obs_min) / (obs_max - obs_min)
-        aux = (aux[0]*self.q_size[0]).astype('uint8'), (aux[1]*self.q_size[1]).astype('uint8')
-        return aux
+        aux = aux*self.q_size
+        return tuple(aux.astype('uint8'))
     
-    def get_action(self, action_space, state: tuple[int,int]) -> int:
-        """
-        Returns the best action with probability (1-epsilon)
+    def get_action(self, obs: tuple[int,int]) -> int:
+        """Returns the best action with probability (1-epsilon)
         otherwise a random action with probability epsilon to ensure exploration
+
+        Args:
+            state (tuple[int,int]): observation
+
+        Returns:
+            int: action
         """
-        
+        state = self.map_obs_to_stat(obs)
         # with probability epsilon return a random action to explore the environment
         if np.random.random() < self.epsilon:
-            return action_space.sample()
+            return self.action_space.sample()
         # with probability (1 - epsilon) act greedily (exploit)
         else:
             return np.argmax(self.q_table[state])
         
     def update(
         self,
-        state: tuple[int, int],
+        obs: tuple[int, int],
         action: int,
         reward: float,
-        next_state: tuple[int, int],
+        next_obs: tuple[int, int],
     ):
         """Updates the Q-value of an action
         Update Q(s,a):= Q(s,a) + lr[R(s,a) + gamma * max(Q(s',a')-Q(s,a))]
@@ -74,6 +86,8 @@ class QLearning:
             reward (float): reward
             next_state (tuple[int, int]): next state
         """
+        state = self.map_obs_to_stat(obs)
+        next_state = self.map_obs_to_stat(next_obs)
         
         future_q_value = np.max(self.q_table[next_state])
         delta = (
@@ -92,6 +106,7 @@ class QLearning:
         
         
     def reset_qtable(self):
-        """Reset the Q-table"""
-
-        self.q_table = np.zeros(shape=(self.q_size))
+        """Reset the Q-table = 0"""
+        act_size = self.action_space.n
+        q_size = tuple(size for size in (self.q_size+(act_size,)))
+        self.q_table = np.zeros(shape=q_size)
--- QLearning/main.py
+++ QLearning/main.py
@@ -34,7 +34,7 @@ print(f'Observation:{observation} \nReward:{reward}\nTerminated:{terminated}')
 
 ##hiperparameters
 learning_rate = 0.02
-n_episodes = 10000
+n_episodes = 100
 start_epsilon = 1.0
 epsilon_decay = start_epsilon / (n_episodes/2)
 final_epsilon = 0.05
@@ -42,76 +42,74 @@ final_epsilon = 0.05
 env = gym.wrappers.RecordVideo (env=env, 
                                 video_folder='QLearning/tmp',
                                 name_prefix='test-video',
-                                episode_trigger=lambda x: (x-1)%3000 == 0)
+                                episode_trigger=lambda x: (x-1)%3 == 0)
 
 env = gym.wrappers.RecordEpisodeStatistics(env=env, deque_size=n_episodes)
-
-
-
-obs_min = env.observation_space.low
-obs_max = env.observation_space.high
-
 agent = QLearning(
     learning_rate = learning_rate,
     initial_epsilon = start_epsilon,
     epsilon_decay = epsilon_decay,
     final_epsilon = final_epsilon,
+    observation_space=observation_space,
+    action_space = action_space,
 )
+print(agent.get_action(observation))
 
-for episode in tqdm(range(n_episodes)):
-    state, info = env.reset()
-    state = agent.map_obs_to_stat(state, obs_max, obs_min)
-    done = False
-    while not done:
-        action = agent.get_action(env.action_space, state)
-        next_state, reward, terminated, truncated, info = env.step(action)
-        next_state = agent.map_obs_to_stat(next_state, obs_max, obs_min)
-        #update the agent
-        agent.update(state, action, reward, next_state)
-        #update if the environment is done and the current state
-        
-        done = terminated or truncated
-        state = next_state
-            
-    agent.decay_epsilon()
-
-
-
-
-rolling_length = 500
-fig, axs = plt.subplots(ncols=3, figsize=(12,5))
-axs[0].set_title("Episode rewards")
-reward_moving_average = (
-    np.convolve(
-        np.array(env.return_queue).flatten(), np.ones(rolling_length), mode='valid'
-    )
-    / rolling_length
-)
-
-axs[0].plot(range(len(reward_moving_average)),reward_moving_average)
-axs[1].set_title("Episode lengths")
-
-length_moving_average = (
-    np.convolve(
-        np.array(env.length_queue).flatten(), np.ones(rolling_length), mode='same'
-    )
-    / rolling_length
-)
-
-axs[1].plot(range(len(length_moving_average)), length_moving_average)
-axs[2].set_title("Training Error")
-
-training_error_moving_average = (
-    np.convolve(
-        np.array(agent.training_error), np.ones(rolling_length), mode='same'
-    )
-    /rolling_length
-)
-
-axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
-plt.tight_layout()
-plt.show()
+#for episode in tqdm(range(n_episodes)):
+#    state, info = env.reset()
+#    state = agent.map_obs_to_stat(state, obs_max, obs_min)
+#    done = False
+#    while not done:
+#        action = agent.get_action(env.action_space, state)
+#        next_state, reward, terminated, truncated, info = env.step(action)
+#        next_state = agent.map_obs_to_stat(next_state, obs_max, obs_min)
+#        #update the agent
+#        agent.update(state, action, reward, next_state)
+#        #update if the environment is done and the current state
+#        
+#        done = terminated or truncated
+#        state = next_state
+#            
+#    agent.decay_epsilon()
+#
 
 
-
-
+#
+#rolling_length = 500
+#fig, axs = plt.subplots(ncols=3, figsize=(12,5))
+#axs[0].set_title("Episode rewards")
+#reward_moving_average = (
+#    np.convolve(
+#        np.array(env.return_queue).flatten(), np.ones(rolling_length), mode='valid'
+#    )
+#    / rolling_length
+#)
+#
+#axs[0].plot(range(len(reward_moving_average)),reward_moving_average)
+#axs[1].set_title("Episode lengths")
+#
+#length_moving_average = (
+#    np.convolve(
+#        np.array(env.length_queue).flatten(), np.ones(rolling_length), mode='same'
+#    )
+#    / rolling_length
+#)
+#
+#axs[1].plot(range(len(length_moving_average)), length_moving_average)
+#axs[2].set_title("Training Error")
+#
+#training_error_moving_average = (
+#    np.convolve(
+#        np.array(agent.training_error), np.ones(rolling_length), mode='same'
+#    )
+#    /rolling_length
+#)
+#
+#axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+#plt.tight_layout()
+#plt.show()
+#
+#
+#
+#
+#
